14ef207a7a616350423e8ca2ea510782c1869a83
elias <elias.saire@ynov.com>
2025-12-05 13:39:54 +0100

    feat: add URL handler for /url

--- main.go
+++ main.go
@@ -7,4 +7,9 @@ import (
 
 func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
+http.HandleFunc("/url", UrlHandler)
+
+}
+func UrlHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<h1>URL visited:</h1><p>%s</p>", r.URL.Path)
 }
